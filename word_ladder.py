9e2917fd584060349125f3ea932b691dcb1f5ad6
benfig1127 <benjifig@gmail.com>
2021-01-22 22:47:59 -0800

    fixed bug??

--- word_ladder.py
+++ word_ladder.py
@@ -2,40 +2,13 @@
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
-	'''
-	Returns a list satisfying the following properties:
 
-	1. the first element is `start_word`
-	2. the last element is `end_word`
-	3. elements at index i and i+1 are `_adjacent`
-	4. all elements are entries in the `dictionary_file` file
-
-	For example, running the command
-	```
-	word_ladder('stone','money')
-	```
-	may give the output
-	```
-	['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
-	```
-	but the possible outputs are not unique,
-	so you may also get the output
-	```
-	['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
-	```
-	(We cannot use doctests here because the outputs are not unique.)
-
-	Whenever it is impossible to generate a word ladder between the two words,
-	the function returns `None`.
-	'''
-	
 	#read in dictionary file 
 	word_list=[]
 	f = open("words5.dict", "r")
 	for line in f:
 		word_list.append(line.strip())
-	
-	
+
 	if start_word==end_word:
 		return [start_word]
 	
@@ -49,9 +22,6 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
 	while queue:
 		current_stack=queue.pop(0)
 		
-		#print('top of stack:',top_of_stack)
-		#print('bottom of stack:', current_stack[0])
-			
 		
 		for dict_word in word_list:
 			
@@ -61,12 +31,12 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
 					current_stack.append(dict_word)
 					
 					#removes duplicate entries
-					cleaned_output=[]
-					for i in current_stack:
-						if i not in cleaned_output:
-							cleaned_output.append(i)
+					#cleaned_output=[]
+					#for i in current_stack:
+						#if i not in cleaned_output:
+							#cleaned_output.append(i)
 					
-					return cleaned_output
+					return current_stack
 				
 				copy_of_current_stack=current_stack.copy()
 				copy_of_current_stack.append(dict_word)
@@ -122,15 +92,15 @@ def _adjacent(word1, word2):
 		
 	
 	
-#print(word_ladder('stone','stone'))
+print(word_ladder('stone','stone'))
 
-#print(word_ladder('aloof','aloof'))
+print(word_ladder('aloof','aloof'))
 	
 
-#print(word_ladder('babes','child'))	
+print(word_ladder('babes','child'))	
 	
 	
-#print(word_ladder('child','babes'))	
+print(word_ladder('child','babes'))	
 	
 	
 	
